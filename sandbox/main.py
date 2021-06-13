ee50a6a5d33d3eeac97802faf85a24fddb53ebc4
masanori0209 <masanorimurakoshi0209@gmail.com>
2021-06-13 11:11:50 +0900

    flask fix

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,9 +1,40 @@
-from classes import *
+from flask import Flask, make_response
+from Queue import Queue
+import time
+import functools
 
-cls = globals()["ClassA"]
-instance = cls("A1", "B1")
-instance.print()
+app = Flask(__name__, static_folder=None)
+queueing = Queue(maxsize=os.envget("QUEUE_SIZE"))
 
-cls = globals()['ClassB']
-instance = cls("A2", "B2")
-instance.print()
+def multiple_control(q):
+    def _multiple_control(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            q.put(time.time())
+            result = func(*args,**kwargs)
+            q.get()
+            q.task_done()
+            return result
+
+        return wrapper
+    return _multiple_control
+
+@app.route('/', methods=['GET'])
+@multiple_control(singleQueue)
+def root():
+    print("[GET] root")
+    return(make_response('end root'))
+
+@app.route('/slow', methods=['GET'])
+@multiple_control(singleQueue)
+def slow():
+    print('[GET] slow')
+    time.sleep(10)
+    return(make_response('end slow'))
+
+if __name__ == "__main__":
+    app.run(
+        host='localhost',
+        port=3000,
+        threaded=True
+    )
